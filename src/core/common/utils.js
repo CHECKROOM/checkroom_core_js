5c48f4d089bfd8cbb1df5bf88d69333ca8f692ff
Jeroen Verhoest <jeroen@cheqroom.com>
2015-09-24 09:54:33 +0200

    Added getParsedLines helper method to common

--- src/core/common/utils.js
+++ src/core/common/utils.js
@@ -1,7 +1,7 @@
 /*
  * Util helpers
  */
-define(function () {
+define(['jquery'], function ($) {
     return {
         /**
          * Turns an integer into a compact text to show in a badge
@@ -55,6 +55,32 @@ define(function () {
             var regex = new RegExp( regexS );
             var results = regex.exec( window.location.href );
             return (results) ? decodeURIComponent(results[1].replace(/\+/g, " ")) : null;
-        }       
+        },
+        /**
+         * getParsedLines 
+         *
+         * @memberOf common
+         * @name  common#getParsedLines
+         * @method
+         * 
+         * @param  {string} text 
+         * @return {Array}      
+         */
+        getParsedLines: function(text){
+            if( (text) &&
+                (text.length > 0)) {
+                var customs = text.split(/\s*([,;\r\n]+|\s\s)\s*/);
+                return customs.filter(function(cust, idx, arr) {
+                    return (
+                        (cust.length>0) &&
+                        (cust.indexOf(',')<0) &&
+                        (cust.indexOf(';')<0) &&
+                        ($.trim(cust).length>0) &&
+                        (arr.indexOf(cust) >= idx));
+                    });
+            }else{
+                return [];
+            }
+        }      
     };
 });

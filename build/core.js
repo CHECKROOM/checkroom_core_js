5f44ce51eac623b73cee3ec97136117d2ad3fc16
Jeroen Verhoest <jeroen@cheqroom.com>
2015-12-02 11:33:08 +0100

    Added kit status helper

--- build/core.js
+++ build/core.js
@@ -2015,7 +2015,51 @@ common_slimdown = function () {
   window.Slimdown = Slimdown;
 }();
 common_kit = function ($) {
-  return {};
+  return {
+    /**
+     * getKitStatus 
+     * Available 		=> if all items in the kit are available
+        * Checking out 	=> if 1 item in the kit is checking out
+        * Checked out 		=> if 1 item in the kit is checked out 
+        * Expired			=> if 1 item in the kit is expired
+        * 
+     * @memberOf common
+     * @name  common#getKitStatus
+     * @method
+     * 
+     * @param  status
+     * @return {string}        
+     */
+    getKitStatus: function (items) {
+      var statuses = {};
+      var itemStatuses = [];
+      // Make dictionary of different item statuses
+      $.each(items, function (i, item) {
+        if (!_statuses[item.status]) {
+          _statuses[item.status] = true;
+          itemStatuses.push(item.status);
+        }
+      });
+      // First check if all items in the kit are available
+      // if not check by status importance 
+      // Expired > Checked out > checking out
+      if (itemStatuses.length == 1 && itemStatuses['available']) {
+        // all items in the kit are available
+        return 'available';
+      } else if (itemStatuses['expired']) {
+        // 1 item in the kit is expired
+        return 'expired';
+      } else if (itemStatuses['checkedout']) {
+        // 1 item in the kit is checked out
+        return 'checkedout';
+      } else if (itemStatuses['await_checkout']) {
+        // 1 item in the kit is awaiting checkout
+        return 'await_checkout';
+      } else {
+        return 'unknown';
+      }
+    }
+  };
 }(jquery);
 common = function ($, code, order, reservation, item, conflicts, keyvalues, image, attachment, inflection, validation, utils, slimdown, kit) {
   /**
--- src/core/common/kit.js
+++ src/core/common/kit.js
@@ -9,8 +9,7 @@ define(['jquery'], function ($) {
 	     * Checking out 	=> if 1 item in the kit is checking out
 	     * Checked out 		=> if 1 item in the kit is checked out 
 	     * Expired			=> if 1 item in the kit is expired
-	     * Incomplete 		=> if 1 item in the kit is at a different location
-		 * 
+	     * 
 		 * @memberOf common
 		 * @name  common#getKitStatus
 		 * @method
@@ -18,34 +17,36 @@ define(['jquery'], function ($) {
 		 * @param  status
 		 * @return {string}        
 		 */
-		/*getKitStatus: function(items) {
+		getKitStatus: function(items) {
 	        var statuses = {};
-            var orders = {};
-            var locations = {};
-
             var itemStatuses = [];
-            var itemOrders = [];
-            var itemLocations = []; 
-
+            
+            // Make dictionary of different item statuses
             $.each(items, function(i, item){  
                 if(!_statuses[item.status]){  
                     _statuses[item.status] = true;
                     itemStatuses.push(item.status);
-                } 
-
-                if(!_orders[item.order]){
-                    _orders[item.order] = true;
-                    itemOrders.push(item.order);
                 }
             });
 
-             //all items in the kit have the same status AND
-            //all items are in the same order or arn't in an order
-            if(itemStatuses.length == 1 && itemOrders.length <= 1){
-                return itemStatuses[0];
+            // First check if all items in the kit are available
+            // if not check by status importance 
+            // Expired > Checked out > checking out
+            if(itemStatuses.length == 1 && itemStatuses["available"]){
+                // all items in the kit are available
+                return "available";
+            }else if (itemStatuses["expired"]){
+                // 1 item in the kit is expired
+                return "expired";
+            }else if (itemStatuses["checkedout"]){
+                // 1 item in the kit is checked out
+                return "checkedout";
+            }else if (itemStatuses["await_checkout"]){
+                // 1 item in the kit is awaiting checkout
+                return "await_checkout";
             }else{
-                return "incomplete";
+                return "unknown";
             }
-	    }*/
+	    }
 	};
 });

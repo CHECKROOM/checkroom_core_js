9135893d164a36f6616bf2e257026e9ea11ba529
Jeroen Verhoest <jeroen@cheqroom.com>
2017-03-07 11:34:03 +0100

    Added option to containsValue method to specify fields to search on

--- build/core.js
+++ build/core.js
@@ -3862,31 +3862,55 @@ common_document = {
    * 
    * @param  doc
    * @param  value
+   * @param  fields  
    * @return Boolean       
    */
-  containsValue: function (doc, value) {
+  containsValue: function (doc, value, fields) {
     doc = doc || {};
+    fields = fields || [
+      'name',
+      'fields',
+      'codes',
+      'barcodes'
+    ];
     // Trim search
     value = $.trim(value).toLowerCase();
     // Name contains value?
-    if (doc.name && doc.name.toLowerCase().indexOf(value) != -1)
-      return true;
-    // Field contains value?
-    if (doc.fields) {
-      var findValue = Object.values(doc.fields).find(function (fieldValue) {
-        return fieldValue.toString().indexOf(value) != -1;
-      });
-      if (findValue) {
+    if (fields.indexOf('name') != -1) {
+      if (doc.name && doc.name.toLowerCase().indexOf(value) != -1)
         return true;
+    }
+    // Field contains value?
+    if (fields.indexOf('fields') != -1) {
+      if (doc.fields) {
+        var findValue = Object.values(doc.fields).find(function (fieldValue) {
+          return fieldValue.toString().indexOf(value) != -1;
+        });
+        if (findValue) {
+          return true;
+        }
       }
     }
     // Code contains value?
-    if (doc.codes) {
-      var findValue = doc.codes.find(function (code) {
-        return code.indexOf(value) != -1;
-      });
-      if (findValue) {
-        return true;
+    if (fields.indexOf('codes') != -1) {
+      if (doc.codes) {
+        var findValue = doc.codes.find(function (code) {
+          return code.indexOf(value) != -1;
+        });
+        if (findValue) {
+          return true;
+        }
+      }
+    }
+    // Barcode contains value?
+    if (fields.indexOf('barcodes') != -1) {
+      if (doc.barcodes) {
+        var findValue = doc.barcodes.find(function (code) {
+          return code.indexOf(value) != -1;
+        });
+        if (findValue) {
+          return true;
+        }
       }
     }
     return false;

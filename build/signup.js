bf9407779bf255153668922e0db13925057b31e2
Jeroen Verhoest <jeroen@cheqroom.com>
2017-08-21 14:17:48 +0200

    Added trimLeft/trimRight polyfills

--- build/signup.js
+++ build/signup.js
@@ -2006,6 +2006,18 @@ common_inflection = function () {
       str = padString + str;
     return str;
   };
+  // trimLeft/trimRight polyfill
+  //https://gist.github.com/eliperelman/1036520
+  if (!String.prototype.trimLeft) {
+    String.prototype.trimLeft = function () {
+      return this.replace(/^\s+/, '');
+    };
+  }
+  if (!String.prototype.trimRight) {
+    String.prototype.trimRight = function () {
+      return this.replace(/\s+$/, '');
+    };
+  }
   /**
   * NUMBER EXTENSIONS
   */

8f159db8496087fc0f650449c79544a160f2aa02
Karel Verschraegen <karel@cheqroom.com>
2021-12-09 11:20:26 +0100

    Remove isFreeEmail function from validation

--- src/core/common/validation.js
+++ src/core/common/validation.js
@@ -24,18 +24,6 @@ define(['moment'], function (moment) {
             return ((m!=null) && (m.length>0));
         },
         /**
-         * isFreeEmail
-         * @memberOf common
-         * @name common#isFreeEmail
-         * @method
-         * @param email
-         * @returns {boolean}
-         */
-        isFreeEmail: function(email) {
-            var m = email.match(/^([\w-\+]+(?:\.[\w-\+]+)*)@(?!gmail\.com)(?!yahoo\.com)(?!hotmail\.com)(?!163\.com)(?!qq\.com)(?!mail\.ru)((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,}(?:\.[a-z]{2})?)$/i);
-            return m == null;
-        },
-        /**
          * isValidPhone
          * @memberOf common
          * @name  common#isValidPhone

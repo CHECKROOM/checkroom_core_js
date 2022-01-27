a66567c0f5fd48d2d9d5ba90865254e6cc00e8d5
Karel Verschraegen <karel@cheqroom.com>
2022-01-27 20:58:00 +0100

    Remove unused methods from util

--- src/core/common/utils.js
+++ src/core/common/utils.js
@@ -1,46 +1,6 @@
 var utils = {};
 
 /**
- * Sorts a given fields dict based on a list of fieldDefs
- * @memberOf utils
- * @name utils#sortFields
- * @param {object} fields
- * @param {Array} fieldDefs
- * @param {Boolean} onlyFormFields (optional)
- * @param {int} limit (optional)
- * @return {Array} list of dicts
- */
-utils.sortFields = function (fields, fieldDefs, onlyFormFields, limit) {
-	var sortedFields = [],
-		fieldDef = null,
-		fieldValue = null,
-		fieldText = null;
-
-	// Work on copy of fieldDefs array
-	fieldDefs = fieldDefs.slice();
-
-	// Return only form field definitions?
-	if (onlyFormFields != null) {
-		fieldDefs = fieldDefs.filter(function (def) {
-			return def.form == onlyFormFields;
-		});
-	}
-
-	// Create a Field dict for each field definition
-	for (var i = 0; i < fieldDefs.length; i++) {
-		fieldDef = fieldDefs[i];
-		fieldValue = fields[fieldDef.name];
-		sortedFields.push(Object.assign({ value: fieldValue }, fieldDef));
-
-		if (limit != null && sortedFields.length >= limit) {
-			break;
-		}
-	}
-
-	return sortedFields;
-};
-
-/**
  * Stringifies an object while first sorting the keys
  * Ensures we can use it to check object equality
  * http://stackoverflow.com/questions/16167581/sort-object-properties-and-json-stringify
@@ -99,29 +59,6 @@ utils.badgeify = function (count) {
 	} else {
 		return '';
 	}
-};
-
-/**
- * Turns a firstName lastName into a fistname.lastname login
- * @memberOf utils
- * @name  utils#getLoginName
- * @method
- * @param  {string} firstName
- * @param  {string} lastName
- * @return {string}
- */
-utils.getLoginName = function (firstName, lastName) {
-	var patt = /[\s-]*/gim;
-
-	var parts = [];
-	if (firstName) {
-		parts.push(firstName.latinise().toLowerCase().replace(patt, ''));
-	}
-	if (lastName) {
-		parts.push(lastName.latinise().toLowerCase().replace(patt, ''));
-	}
-
-	return parts.join('.');
 };
 
 /**

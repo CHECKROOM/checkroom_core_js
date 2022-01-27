f1dc45d5e139ef851ca345621174cf596586afee
Karel Verschraegen <karel@cheqroom.com>
2022-01-27 16:05:16 +0100

    Remove isValidUrl from String.prototype

--- src/core/common/inflection.js
+++ src/core/common/inflection.js
@@ -65,33 +65,7 @@ if (!String.prototype.truncate) {
 		return re;
 	};
 }
-if (!String.prototype.isValidUrl) {
-	/**
-	 * isValidUrl
-	 *
-	 * @memberOf String
-	 * @name  String#isValidUrl
-	 * @method
-	 *
-	 * @return {Boolean}
-	 */
-	String.prototype.isValidUrl = function () {
-		var pattern = new RegExp(
-			'^(https?:\\/\\/)?' + // protocol
-				'((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' + // domain name
-				'((\\d{1,3}\\.){3}\\d{1,3}))' + // OR ip (v4) address
-				'(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' + // port and path
-				'(\\?[;&a-z\\d%_.~+=-]*)?' + // query string
-				'(\\#[-a-z\\d_]*)?$',
-			'i'
-		); // fragment locator
-		if (!pattern.test(this)) {
-			return false;
-		} else {
-			return true;
-		}
-	};
-}
+
 if (!String.prototype.hashCode) {
 	/**
 	 * hashCode

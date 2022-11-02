d66d113c4c62998f81d6dcda7a49273d732e179d
Karel Verschraegen <karel@cheqroom.com>
2022-11-02 16:36:33 +0100

    Don't check on profile for non-user-configurable plan features

--- src/core/permissionHandler.js
+++ src/core/permissionHandler.js
@@ -22,7 +22,7 @@ var PermissionHandler = function (user, profile, limits, permissions, isFeatureE
 
 	// Helper booleans that mix a bunch of role stuff and profile / limits stuff
 	this._isBlockedContact = user.customer && user.customer.status == 'blocked';
-	this._useWebhooks = limits.allowWebhooks && profile.useWebhooks;
+	this._useWebhooks = limits.allowWebhooks;
 	this._useOrders = limits.allowOrders && profile.useOrders;
 	this._useReservations = limits.allowReservations && profile.useReservations;
 	this._usePdf = limits.allowGeneratePdf;
@@ -39,10 +39,10 @@ var PermissionHandler = function (user, profile, limits, permissions, isFeatureE
 	this._useRestrictLocations = limits.allowRestrictLocations && profile.useRestrictLocations;
 	this._useReporting = limits.allowReporting && profile.useReporting;
 	this._useDepreciations = limits.allowDepreciations && profile.useDepreciations;
-	this._useNotifications = limits.allowNotifications && profile.useNotifications;
+	this._useNotifications = limits.allowNotifications;
 	this._useBlockContacts = limits.allowBlockContacts && profile.useBlockContacts;
 	this._useReservationsClose = this._useReservations && profile.useReservationsClose;
-	this._useSlack = limits.allowIntegrationSlack && profile.useIntegrationSlack;
+	this._useSlack = limits.allowIntegrationSlack;
 	this._useApi = limits.allowAPI;
 	this._useReleaseAtLocation =
 		this._useCustody && (profile.custodyCanChangeLocation !== undefined ? profile.custodyCanChangeLocation : true); // TODO change this update fallback (mobile)
